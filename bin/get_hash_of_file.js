c1c9097cc560304a8f606af18946f399022340d9
rucadi <ruben.cano96@gmail.com>
2022-04-17 21:09:02 +0200

    add SkyUI

---
+++ bin/get_hash_of_file.js
@@ -0,0 +1,8 @@
+import { exit } from 'process';
+import WebTorrent from 'webtorrent-hybrid'
+const client = new WebTorrent()
+client.seed(process.argv[2], torrent=> 
+    {
+        console.log(torrent.infoHash);
+        exit(0);
+    });
